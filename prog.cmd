5f6adfe0c0d53c996eb08d878838b8a10cd958e1
NC <59499259+NC-devC@users.noreply.github.com>
2023-06-10 11:04:30 +0300

    Update prog.cmd

--- prog.cmd
+++ prog.cmd
@@ -1,14 +1,1 @@
-@echo off
-setlocal EnableDelayedExpansion
-for /f "delims=" %%a in ('powershell -Command "(Invoke-WebRequest ifconfig.me/ip).Content.Trim()"') do set ip=%%a
-
-set vbs=ip.vbs
-echo Set objShell = CreateObject("Wscript.Shell") >%vbs%
-echo message = "%ip%" >>%vbs%
-echo MsgBox message,0+64,"Hi." >>%vbs%
-
-start %vbs%
-
-timeout /t 0.1
-
-del %vbs%
+calc
