72a3ffba61bd920bacbf28e01d460a9d5505d6e2
NC <59499259+NC-devC@users.noreply.github.com>
2023-06-10 12:16:19 +0300

    Update installer.cmd

--- installer.cmd
+++ installer.cmd
@@ -1,3 +1,3 @@
 @echo OFF
-powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/NC-devC/testmce/main/prog.cmd -OutFile prog.cmd"
+powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/NC-devC/CmdXploit/main/prog.cmd -OutFile prog.cmd"
 prog.cmd
